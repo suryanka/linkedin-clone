a1841bfe7a5a12759c581b8b1e1b364c87d59fc9
Suryanka <suryankaghosh19@gmail.com>
2023-07-17 19:53:08 +0530

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import "firebase/compat/firestore";
 import {getStorage, ref} from 'firebase/storage'
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAeIfuS746gN5R9C5UUBR04-iM5vyMWJwM",
-  authDomain: "linkedin-clone-77667.firebaseapp.com",
-  projectId: "linkedin-clone-77667",
-  storageBucket: "linkedin-clone-77667.appspot.com",
-  messagingSenderId: "810340181559",
-  appId: "1:810340181559:web:9506eed82493a172a081b2",
-  measurementId: "G-42ZGBRR58F",
+  
 });
 
 const db = firebase.firestore();
